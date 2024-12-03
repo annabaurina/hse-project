a454f504bac62e1ca6064bd9c7983ef2e646f0f9
annabaurina <baurina.ann@gmail.com>
2024-12-03 20:12:09 +0300

    script for git

---
+++ how_to_git.sh
@@ -0,0 +1,16 @@
+cd C:/hse/курсовая
+git clone https://github.com/annabaurina/hse-project.git
+ls
+cd hse-project
+git branch {branch_name}
+git checkout {branch_name}
+
+#how to commit:
+git add .
+git commit -m "{commit's name}"
+git push
+# or
+git push --set-upstream origin {branch_name}
+
+
+
